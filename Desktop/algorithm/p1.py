d76cef9873553d559b7eb5d547a24ff362e3d43d
selimssy <selimssy@naver.com>
2023-03-22 18:28:28 +0900

    예산

---
+++ Desktop/algorithm/p1.py
@@ -0,0 +1,12 @@
+numbers = [1,3,2,5,4]
+numbers.sort()
+p
+answer = []
+
+for i in range(0, len(numbers)-1):
+    for j in range(i+1, len(numbers)):
+        if numbers[i] + numbers[j] not in answer:
+            answer.append(numbers[i] + numbers[j])
+
+answer.sort()
+print(answer)            
